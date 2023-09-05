45c25217c3efd4e9f63f30e4b0ba9aa9d779678b
Ankit Thakur <2thakurankitsingh@gmail.com>
2023-09-05 18:04:35 +0530

    Palindrome Problem is solve

--- single_link_list/Palindrome Check/Check_palindrome.cpp
+++ single_link_list/Palindrome Check/Check_palindrome.cpp
@@ -1,5 +1,9 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
+
+// https://leetcode.com/problems/palindrome-linked-list/submissions/
 
 class Node {
     public:
@@ -40,22 +44,74 @@ void show(Node* &Head){
     }
     cout<<endl;
     
+
 }
+   Node* reverseList(Node* head){
+        Node* curr = head;
+        Node* forword = NULL;
+        Node* prev  = NULL;
+
+        while(curr!= NULL){
+            forword = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr  = forword;
+        }
+        return prev;
+   }
+    bool isPalindrome(Node* head) {
+        if(head == NULL){
+            return false;
+        }
+        if(head->next == NULL){
+            return true;
+        }
+        
+        vector<int> ans;
+        
+        Node* curr = head;
+
+        while (curr!=NULL)
+        {
+            int value = curr->data;
+            ans.push_back(value);
+            curr = curr->next;
+
+        }
+        curr = head;
+        reverse(ans.begin(),ans.end());
+        int i = 0 ;
+        while (curr!=NULL)
+        {
+            int value = curr->data;
+            if(value != ans[i] ){
+                return false;
+            }
+            i++;
+            curr = curr->next;
+
+        }
+
+        return true;
+        
+    }
 int main(int argc, char const *argv[])
 {
-    Node *Node1 = new Node(5);
+    Node *Node1 = new Node(1);
    
     Node *Head = Node1;
     Node *tail = Node1;
-
-    // cout<<Head->data<<endl;
-    // cout<<tail->data<<endl;
-
-    append(Head,tail,8);
     
-    append(Head,tail,10);
+    append(Head,tail,1);
+    append(Head,tail,2);
+
+    append(Head,tail,1);
+  
+   
 
     show(Head);
+
+    cout<<isPalindrome(Head);
 
     
 
